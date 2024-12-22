3c27fe1196d15c7edd1cc508dbe25e30a227a955
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2024-12-21 20:37:14 -0500

    Add chars frecuency stats

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 def main():
     data = get_data('./books/frankenstein.txt')
     ammount_words = word_count(data)
+    char_count = char_frecuency(data)
     print(ammount_words)
+    print(char_count)
 
 
 def get_data(file):
@@ -9,8 +11,22 @@ def get_data(file):
         return f.read()
 
 
-def word_count(string=''):
+def word_count(string):
     return len(string.split())
+
+
+def char_frecuency(string=''):
+    chars = {}
+    for char in string:
+        if not char.isalpha():
+            continue
+        char_lower = char.lower()
+        if char_lower not in chars:
+            chars[char_lower] = 0
+
+        chars[char_lower] += 1
+
+    return chars
 
 
 main()
